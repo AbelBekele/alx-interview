4e2ec3363a485f841cb4037625dfc52a4a00d48f
Abel <abelbekele.addise@gmail.com>
2023-02-07 22:50:49 +0300

    2nd change

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,45 @@
 #!/usr/bin/node
 
+/**
+ * Star wars api
+ * Script that prints all characters of a Star Wars movie:
+ */
 const request = require('request');
+const filmId = process.argv[2];
+if (!filmId || isNaN(filmId)) {
+  process.exit(1);
+}
+const url = `https://swapi-api.hbtn.io/api/films/${filmId}`;
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const respPromises = [];
+
+  const json = JSON.parse(body);
+  const characters = json.characters;
+
+  characters.forEach((character) => {
+    const url = character;
+    const promise = new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        const json = JSON.parse(body);
+        resolve(json.name);
+      });
+    });
+    respPromises.push(promise);
   });
-};
+  Promise.all(respPromises).then((values) => {
+    values.forEach((value) => {
+      console.log(value);
+    });
+  }).catch((error) => {
+    console.log(error);
+  });
+});
